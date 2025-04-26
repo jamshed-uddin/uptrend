296bd3dc5d73665ce33fb91a234228f14c26b861
Md Jamshed Uddin <jamsheduddin03@gmail.com>
2025-04-26 10:46:29 +0600

    skeleton card adjustment and typo fix

--- src/components/Skeletons.jsx
+++ src/components/Skeletons.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const CardSkeleton = () => {
   return (
-    <div className="animate-pulse flex flex-col gap-2 p-4 border rounded-md shadow-md">
-      <div className="h-32 bg-gray-200 rounded-md"></div>
+    <div className="animate-pulse flex flex-col gap-2  border rounded-md ">
+      <div className="h-80 bg-gray-200 rounded-md"></div>
       <div className="h-4 bg-gray-200 rounded-md w-1/2"></div>
       <div className="h-4 bg-gray-200 rounded-md w-1/3"></div>
       <div className="h-4 bg-gray-200 rounded-md w-full"></div>
@@ -13,8 +13,8 @@ const CardSkeleton = () => {
 
 const CardListSkeleton = () => {
   return (
-    <div>
-      {Array.from({ length: 5 }).map((_, inx) => (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      {Array.from({ length: 4 }).map((_, idx) => (
         <CardSkeleton key={idx} />
       ))}
     </div>
